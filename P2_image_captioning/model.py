05025faf84c3b3a4541f887af3bd81055f7cb8c6
Arnaud Roussel <39624764+arnaud-roussel@users.noreply.github.com>
2020-04-13 16:45:38 -0700

    First task

--- P2_image_captioning/model.py
+++ P2_image_captioning/model.py
@@ -22,11 +22,22 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=2):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=embed_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)
+        self.embed = nn.Embedding(num_embeddings=vocab_size,
+                                  embedding_dim=embed_size)
+        self.lin_out = nn.Linear(hidden_size, vocab_size)
+                            
     def forward(self, features, captions):
-        pass
+        captions_embeded = self.embed(captions[:, :-1])
+        x = torch.cat([features.unsqueeze(1), captions_embeded], dim=1)
+        outputs, _ = self.lstm(x)  # N, L, Hid
+        logits = self.lin_out(outputs) # N, L, Voc
+        return logits
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
